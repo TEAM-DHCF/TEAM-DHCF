6df1601496041f672a31e6f4b51d16f5245183d9
DARK HAT CYBER FORCE - TEAM DHCF <darkhatcyberforcedhcf@gmail.com>
2024-09-29 18:45:40 +0600

    Add files via upload

---
+++ dd.py
@@ -0,0 +1,161 @@
+import os,sys,time,json,random,re,string,platform,base64,uuid,getpass
+
+#-----------color-----------------#
+
+black="\033[0;30m"        # Black
+
+red="\033[0;31m"          # Red
+
+green="\033[0;32m"        # Green
+
+yellow="\033[0;33m"       # Yellow
+
+blue="\033[0;34m"         # Blue
+
+purple="\033[0;35m"       # Purple
+
+cyan="\033[0;36m"         # Cyan
+
+white="\033[0;37m"        # White
+
+
+
+# Bold
+
+bblack="\033[1;30m"       # Black
+
+bred="\033[1;31m"         # Red
+
+bgreen="\033[1;32m"       # Green
+
+byellow="\033[1;33m"      # Yellow
+
+bblue="\033[1;34m"        # Blue
+
+bpurple="\033[1;35m"      # Purple
+
+bcyan="\033[1;36m"        # Cyan
+
+bwhite="\033[1;37m"       # White
+
+
+
+#-------clear def-------#
+
+def clear():
+
+	os.system('pkg install espeak')
+
+	os.system("clear")
+
+from os import system as _DHCF_
+
+print("\033[1;92m{}>..𝐂𝐇𝐄𝐀𝐊𝐈𝐍𝐆 𝐔𝐏𝐃𝐀𝐓𝐄...")
+
+os.system('espeak -a 300 "cheaking.  update.    "')
+
+os.system("pkg install espeak")
+
+print("\033[1;92m{}>..𝐈𝐀𝐌 𝐃𝐃𝐎𝐒 𝐀𝐓𝐓𝐀𝐂𝐊 𝐓𝐎𝐎𝐋𝐒...")
+
+os.system('espeak -a 300 " i am.    d.d.o.s   a.t.t.a.c.k   tools    "')
+
+print("\033[1;92m{}>..I MADE BY TEAM DHCF...")
+
+os.system('espeak -a 300 " i.     made.    by.    TEAM DHCF  .   .    "')
+
+
+
+#os.system("git pull")
+
+#os.system("pip install bs4")
+
+#os.system("pip install requests")
+
+#os.system("pip install mechanize")
+
+
+
+print("\033[1;92m{}>.. Tarmux  (𝐑𝐎𝐁𝐎𝐓) 𝐒𝐘𝐒𝐓𝐄𝐌 𝐈𝐍𝐒𝐓𝐀𝐋𝐈𝐍𝐆....\033[1;30m")
+
+os.system('espeak -a 300 "Tarmux   Robot.   system.   installing "')
+
+os.system("git pull")
+
+print("\033[1;92m{}>..𝐑𝐎𝐁𝐎𝐓 𝐈𝐍𝐒𝐓𝐀𝐋𝐋 𝐂𝐎𝐌𝐏𝐋𝐄𝐓𝐄..{√}\033[1;30m")
+
+os.system('espeak -a 300 "Robot.   install.   complete"')
+
+print("\033[1;92m{}>..𝐏𝐋𝐄𝐀𝐒𝐄 𝐖𝐀𝐈𝐓 𝐅𝐎𝐑 𝐋𝐎𝐀𝐃𝐈𝐍𝐆.....\033[1;30m")
+
+os.system('espeak -a 300 "Please.   WAIT.   FOR.   LOADING...."')
+
+#os.system("pip install bs4")
+
+import os,requests,time
+
+time.sleep(1)
+
+os.system("clear")
+
+
+
+def main():
+
+	os.system('clear')
+
+os.system('clear')
+
+
+
+logo = ("""\033[1;37m
+
+
+\033[1;32m /$$$$$$$  /$$   /$$  /$$$$$$  /$$$$$$$$
+\033[1;32m| $$__  $$| $$  | $$ /$$__  $$| $$_____/
+\033[1;32m| $$  \ $$| $$  | $$| $$  \__/| $$      
+\033[1;32m| $$  | $$| $$$$$$$$| $$      | $$$$$   
+\033[1;32m| $$  | $$| $$__  $$| $$      | $$__/   
+\033[1;32m| $$  | $$| $$  | $$| $$    $$| $$      
+\033[1;32m| $$$$$$$/| $$  | $$|  $$$$$$/| $$      
+\033[1;32m|_______/ |__/  |__/ \______/ |__/\033[1;31m [\033[1;36mBangladeshi TM\033[1;31m]\033[1;31m
+
+═══════════════════════════════════════════════\033[1;37m
+ Author      : TEAM DHCF
+ Tool Status : PAID
+ Tools Name  : DDOS-ATTACK
+ Network     : DATA/WIFI (Both)
+ Github      : github.com/TEAM-DHCF\033[1;31m
+═══════════════════════════════════════════════\033[1;37m""")
+
+class Main:
+		os.system("clear")
+		print(logo)
+		print("\033[1;37m [1] DDOS-ATTACK")
+		print(" [2] Our Facebook Group")
+		print(" [3] Our Facebook Page")
+		print(" [4] Our More Tools")
+		print(" [5] Exit This Tools")
+		print("")
+		DHCF =input("\033[1;36m [>_] \033[1;33m")
+		if DHCF in ["file", "File"]:
+			os.system("xdg-open https://m.facebook.com/groups/dark.hat.cyber.force/")
+			os.system("git clone https://github.com/TEAM-DHCF/TEAM-DHCF && cd TEAM-DHCF && python 'main (1)'.py")
+		if DHCF in ["1", "01"]:
+			os.system("xdg-open https://m.facebook.com/groups/dark.hat.cyber.force/")
+			sys.exit()
+		if DHCF in ["5", "05"]:
+			os.system("xdg-open https://m.facebook.com/groups/dark.hat.cyber.force/")
+			sys.exit()
+		if DHCF in ["6", "06"]:
+		  os.system("xdg-open https://www.facebook.com/dark.hat.cyber.force")
+		  sys.exit()
+		if DHCF in ["7", "07"]:
+			os.system("xdg-open https://github.com/TEAM-DHCF?tab=repositories")
+			sys.exit()
+		if DHCF in ["8", "08"]:
+		  sys.exit("Exit Successfully")
+		else:
+			print (" Select Correctly ")
+			time.sleep(1)
+Main()
